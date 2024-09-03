2b588ad5c729c7e24c59bf4532712b9d9c85fbb7
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-09-03 22:58:44 +0200

    Prepare initial project release

--- lib/file_rewriter/test/test__file_rewriter.ml
+++ lib/file_rewriter/test/test__file_rewriter.ml
@@ -13,3 +13,271 @@
 (*  See the License for the specific language governing permissions and       *)
 (*  limitations under the License.                                            *)
 (******************************************************************************)
+
+let path = Fpath.v "foo.txt"
+
+let original_contents = {|
+Hello World
+Hello Newline
+|} |> String.strip
+
+let%expect_test "insert" =
+  let file_rewriter = File_rewriter.create ~path:(Fpath.v "foo.txt") ~original_contents in
+  let reset () = File_rewriter.reset file_rewriter in
+  let test () = print_endline (File_rewriter.contents file_rewriter) in
+  (* Applying no substitution shall return the original contents. *)
+  require_equal
+    [%here]
+    (module String)
+    original_contents
+    (File_rewriter.contents file_rewriter);
+  [%expect {||}];
+  test ();
+  [%expect {|
+    Hello World
+    Hello Newline |}];
+  (* Inserting at invalid offsets raises [Invalid_argument]. *)
+  require_does_raise [%here] (fun () ->
+    File_rewriter.insert file_rewriter ~offset:(-1) ~text:"Prefix\n");
+  [%expect {| (Invalid_argument File_rewriter.insert) |}];
+  require_does_raise [%here] (fun () ->
+    File_rewriter.insert
+      file_rewriter
+      ~offset:(String.length original_contents + 1)
+      ~text:"Prefix\n");
+  [%expect {| (Invalid_argument File_rewriter.insert) |}];
+  (* We shall be able to apply a subst from the very beginning. *)
+  File_rewriter.insert file_rewriter ~offset:0 ~text:"Prefix\n";
+  test ();
+  [%expect {|
+    Prefix
+    Hello World
+    Hello Newline |}];
+  (* But we don't have to, in which case the first part shall not be truncated. *)
+  reset ();
+  File_rewriter.insert file_rewriter ~offset:6 ~text:"Friendly ";
+  test ();
+  [%expect {|
+    Hello Friendly World
+    Hello Newline |}];
+  (* It is supported to insert several texts at the same offset. When doing so,
+     the order of insertions shall be respected, and insertion requested first
+     are inserted first in left-to-right order. *)
+  File_rewriter.insert file_rewriter ~offset:6 ~text:"Awesome ";
+  test ();
+  [%expect {|
+    Hello Friendly Awesome World
+    Hello Newline
+    |}];
+  File_rewriter.insert
+    file_rewriter
+    ~offset:(String.length "Hello World\n")
+    ~text:"Inserting a newline\n";
+  test ();
+  [%expect
+    {|
+    Hello Friendly Awesome World
+    Inserting a newline
+    Hello Newline
+    |}];
+  (* Inserting at the very end shall be supported too. *)
+  File_rewriter.insert file_rewriter ~offset:(String.length original_contents) ~text:"...";
+  test ();
+  [%expect
+    {|
+    Hello Friendly Awesome World
+    Inserting a newline
+    Hello Newline...
+    |}];
+  ()
+;;
+
+let%expect_test "replace" =
+  let file_rewriter = File_rewriter.create ~path ~original_contents in
+  let reset () = File_rewriter.reset file_rewriter in
+  let test () = print_endline (File_rewriter.contents file_rewriter) in
+  File_rewriter.remove
+    file_rewriter
+    ~range:{ start = String.length "Hello"; stop = String.length "Hello World" };
+  test ();
+  [%expect {|
+    Hello
+    Hello Newline
+    |}];
+  reset ();
+  File_rewriter.replace
+    file_rewriter
+    ~range:{ start = String.length "Hello "; stop = String.length "Hello World" }
+    ~text:"You";
+  test ();
+  [%expect {|
+    Hello You
+    Hello Newline
+    |}];
+  (* The order between insertions and replaces shall not matter, the insertions
+     are going to be placed first. *)
+  File_rewriter.insert file_rewriter ~offset:6 ~text:"Awesome ";
+  test ();
+  [%expect {|
+    Hello Awesome You
+    Hello Newline
+    |}];
+  reset ();
+  (* Indeed, here we show that applying the insert first does yield the same result. *)
+  File_rewriter.insert file_rewriter ~offset:6 ~text:"Awesome ";
+  File_rewriter.replace
+    file_rewriter
+    ~range:{ start = String.length "Hello "; stop = String.length "Hello World" }
+    ~text:"You";
+  test ();
+  [%expect {|
+    Hello Awesome You
+    Hello Newline
+    |}];
+  reset ();
+  (* However, if you try inserting a text into a part that was removed, this is
+     not going to work. *)
+  File_rewriter.replace
+    file_rewriter
+    ~range:{ start = 0; stop = String.length "Hello World" }
+    ~text:"Hi, You!";
+  test ();
+  [%expect {|
+    Hi, You!
+    Hello Newline
+    |}];
+  require_does_raise [%here] (fun () ->
+    File_rewriter.insert file_rewriter ~offset:(String.length "Hello ") ~text:"Awesome ";
+    test ());
+  [%expect
+    {|
+    (File_rewriter.Invalid_rewrites foo.txt
+      ((start 0) (stop 11) (replace_by "Hi, You!"))
+      ((start 6) (stop 6)  (replace_by "Awesome ")))
+    |}];
+  reset ();
+  ()
+;;
+
+let%expect_test "invalid rewrites" =
+  let file_rewriter = File_rewriter.create ~path ~original_contents in
+  let reset () = File_rewriter.reset file_rewriter in
+  let test () = print_endline (File_rewriter.contents file_rewriter) in
+  reset ();
+  (* The order in which [replace] request are performed doesn't matter, as long
+     as the rewrites do not overlap. For example, the following is valid: *)
+  File_rewriter.replace
+    file_rewriter
+    ~range:{ start = 2; stop = String.length "Hello World" }
+    ~text:"You!";
+  File_rewriter.replace file_rewriter ~range:{ start = 0; stop = 2 } ~text:"Hi, ";
+  test ();
+  [%expect {|
+    Hi, You!
+    Hello Newline
+    |}];
+  reset ();
+  (* Rejected cases include:
+
+     - 1. Inserting a text within a part that was rewritten or removed.
+     - 2. Consecutive rewrites with overlapping offsets.
+  *)
+  (* 1. Inserting a text within a part that was rewritten or removed. *)
+  File_rewriter.replace
+    file_rewriter
+    ~range:{ start = 0; stop = String.length "Hello" }
+    ~text:"Hi";
+  test ();
+  [%expect {|
+    Hi World
+    Hello Newline
+    |}];
+  (* This may be counter intuitive, but the offset points to character in the
+     original text, not the text being rewritten. For example, the following
+     insertion request is invalid, because after the first replace that was
+     performed, the current offset to the original text is at char 5, thus
+     inserting at char 3 fails. *)
+  File_rewriter.insert file_rewriter ~offset:3 ~text:"Big ";
+  require_does_raise [%here] (fun () -> test ());
+  [%expect
+    {|
+    (File_rewriter.Invalid_rewrites foo.txt
+      ((start 0) (stop 5) (replace_by Hi))
+      ((start 3) (stop 3) (replace_by "Big ")))
+    |}];
+  reset ();
+  (* Here is a suitable solution for what was attempted above: *)
+  File_rewriter.replace
+    file_rewriter
+    ~range:{ start = 0; stop = String.length "Hello" }
+    ~text:"Hi";
+  File_rewriter.insert file_rewriter ~offset:(String.length "Hello ") ~text:"Big ";
+  test ();
+  [%expect {|
+    Hi Big World
+    Hello Newline
+    |}];
+  (* 2. Consecutive rewrites with overlapping offsets. *)
+  reset ();
+  File_rewriter.replace
+    file_rewriter
+    ~range:{ start = 0; stop = String.length "Hello" }
+    ~text:"Hi";
+  test ();
+  [%expect {|
+    Hi World
+    Hello Newline
+    |}];
+  (* Similarly to what we saw with an invalid offset on insertion, the following
+     doesn't work since the ongoing buffer is already pointing at character 5,
+     and thus position 3 is overlapping. *)
+  File_rewriter.replace
+    file_rewriter
+    ~range:{ start = String.length "Hi "; stop = String.length "Hi World" }
+    ~text:"Universe";
+  require_does_raise [%here] (fun () -> test ());
+  [%expect
+    {|
+    (File_rewriter.Invalid_rewrites foo.txt
+      ((start 0) (stop 5) (replace_by Hi))
+      ((start 3) (stop 8) (replace_by Universe)))
+    |}];
+  ()
+;;
+
+let%expect_test "contents_result" =
+  let file_rewriter = File_rewriter.create ~path ~original_contents in
+  let reset () = File_rewriter.reset file_rewriter in
+  reset ();
+  (* The order in which [replace] request are performed doesn't matter, as long
+     as the rewrites do not overlap. For example, the following is valid: *)
+  File_rewriter.replace
+    file_rewriter
+    ~range:{ start = 2; stop = String.length "Hello World" }
+    ~text:"You!";
+  File_rewriter.replace file_rewriter ~range:{ start = 0; stop = 2 } ~text:"Hi, ";
+  let () =
+    match File_rewriter.contents_result file_rewriter with
+    | Ok contents -> print_endline contents
+    | Error _ -> assert false
+  in
+  [%expect {|
+    Hi, You!
+    Hello Newline
+    |}];
+  File_rewriter.insert file_rewriter ~offset:3 ~text:"Big ";
+  let () =
+    match File_rewriter.contents_result file_rewriter with
+    | Ok (_ : string) -> assert false
+    | Error invalid_rewrites ->
+      print_s [%sexp (invalid_rewrites : File_rewriter.Invalid_rewrites.t)]
+  in
+  [%expect
+    {|
+    ((path foo.txt)
+     (rewrites_with_overlap (
+       ((start 2) (stop 11) (replace_by You!))
+       ((start 3) (stop 3)  (replace_by "Big ")))))
+    |}];
+  ()
+;;
--- lib/sexps_rewriter/test/test__sexps_rewriter.ml
+++ lib/sexps_rewriter/test/test__sexps_rewriter.ml
@@ -13,3 +13,226 @@
 (*  See the License for the specific language governing permissions and       *)
 (*  limitations under the License.                                            *)
 (******************************************************************************)
+
+(* We test the library by using it to apply rewrites to a dune file. *)
+
+(* [Sexps_rewriter] doesn't need to access the disk, but we're saving files on
+   disk because [Err] can show file parts when displaying errors if the location
+   points to an existing file, which makes the test nicer and more
+   understandable. *)
+let save_file ~path ~contents =
+  let paths = String.split (Fpath.to_string path) ~on:'/' in
+  let rec aux dir = function
+    | [] | [ _ ] -> ()
+    | item :: (_ :: _ as tl) ->
+      let dir = dir ^ "/" ^ item in
+      Unix.mkdir dir 0o777;
+      aux dir tl
+  in
+  aux "." paths;
+  Out_channel.with_open_bin (Fpath.to_string path) (fun oc ->
+    Out_channel.output_string oc contents)
+;;
+
+let original_contents =
+  {|
+; Hey this is a comment, which we should preserve !!!
+(library
+  (name sexps_rewriter_test)
+  (flags -w +a-4-40-42-44-66 -warn-error +a)
+  (libraries
+   expect_test_helpers_core
+   base
+   sexps_rewriter
+   file_rewriter
+   parsexp)
+  ; This is another comment.
+  (inline_tests)
+  (lint (pps ppx_js_style -check-doc-comments))
+  (preprocess
+   (pps ppx_sexp_conv ppx_sexp_value)))
+|}
+  |> String.strip
+;;
+
+let%expect_test "libraries sorting" =
+  let path = Fpath.v "path/lib/dune" in
+  let sexps_rewriter =
+    match Sexps_rewriter.create ~path ~original_contents with
+    | Ok t -> t
+    | Error { loc = _; message = _ } -> assert false
+  in
+  let () =
+    let sexps, (_ : Parsexp.Positions.t) =
+      Sexps_rewriter.Private.parser_result sexps_rewriter
+    in
+    (* How many sexps did we just parse here? *)
+    print_s [%sexp { num_sexps = (List.length sexps : int) }];
+    [%expect {| ((num_sexps 1)) |}]
+  in
+  (* If we do nothing, the output shall be equal to that with which we started. *)
+  require_equal
+    [%here]
+    (module String)
+    original_contents
+    (Sexps_rewriter.contents sexps_rewriter);
+  [%expect {||}];
+  let print_diff () =
+    let modified_contents = Sexps_rewriter.contents sexps_rewriter in
+    Expect_test_patdiff.print_patdiff original_contents modified_contents ~context:3
+  in
+  print_diff ();
+  [%expect {||}];
+  (* Let's start with a small experiment: we will rename "name" to "public_name". *)
+  Sexps_rewriter.visit sexps_rewriter ~f:(fun sexp ~range ~file_rewriter ->
+    match sexp with
+    | Atom "name" ->
+      File_rewriter.replace file_rewriter ~range ~text:"public_name";
+      Break
+    | _ -> Continue);
+  print_diff ();
+  [%expect
+    {|
+    -1,6 +1,6
+      ; Hey this is a comment, which we should preserve !!!
+      (library
+    -|  (name sexps_rewriter_test)
+    +|  (public_name sexps_rewriter_test)
+        (flags -w +a-4-40-42-44-66 -warn-error +a)
+        (libraries
+         expect_test_helpers_core
+    |}];
+  (* Here, we reorder the libraries alphabetically. We are doing that by adding
+     a substitution for all of the library names, one by one. There might be
+     simpler way to do this, but this is a good test for [file_rewriter]. *)
+  Sexps_rewriter.visit sexps_rewriter ~f:(fun sexp ~range:_ ~file_rewriter ->
+    match sexp with
+    | List (Atom "libraries" :: libraries) ->
+      let reordered =
+        List.map libraries ~f:(function
+          | Atom name -> name
+          | List _ -> assert false)
+        |> List.sort ~compare:String.compare
+      in
+      List.iter2_exn libraries reordered ~f:(fun sexp name ->
+        File_rewriter.replace
+          file_rewriter
+          ~range:(Sexps_rewriter.range sexps_rewriter sexp)
+          ~text:name);
+      Break
+    | _ -> Continue);
+  print_diff ();
+  [%expect
+    {|
+    -1,13 +1,13
+      ; Hey this is a comment, which we should preserve !!!
+      (library
+    -|  (name sexps_rewriter_test)
+    +|  (public_name sexps_rewriter_test)
+        (flags -w +a-4-40-42-44-66 -warn-error +a)
+        (libraries
+    +|   base
+         expect_test_helpers_core
+    -|   base
+    -|   sexps_rewriter
+    -|   file_rewriter
+    -|   parsexp)
+    +|   file_rewriter
+    +|   parsexp
+    +|   sexps_rewriter)
+        ; This is another comment.
+        (inline_tests)
+        (lint (pps ppx_js_style -check-doc-comments))
+    |}];
+  (* Let's show the end result. *)
+  print_endline (Sexps_rewriter.contents sexps_rewriter);
+  [%expect
+    {|
+    ; Hey this is a comment, which we should preserve !!!
+    (library
+      (public_name sexps_rewriter_test)
+      (flags -w +a-4-40-42-44-66 -warn-error +a)
+      (libraries
+       base
+       expect_test_helpers_core
+       file_rewriter
+       parsexp
+       sexps_rewriter)
+      ; This is another comment.
+      (inline_tests)
+      (lint (pps ppx_js_style -check-doc-comments))
+      (preprocess
+       (pps ppx_sexp_conv ppx_sexp_value)))
+    |}];
+  (* Now we're demonstrating another use case for this library, and that being
+     an engine to produce linting errors and warnings.
+
+     For the sake of the example here, let's assume we want to produce a warning
+     if a certain library appears in the list of dependencies. *)
+  save_file ~path ~contents:original_contents;
+  Sexps_rewriter.visit sexps_rewriter ~f:(fun sexp ~range:_ ~file_rewriter:_ ->
+    match sexp with
+    | List (Atom "libraries" :: libraries) ->
+      List.iter libraries ~f:(function
+        | Atom ("sexps_rewriter" as library_name) as sexp ->
+          let loc = Sexps_rewriter.loc sexps_rewriter sexp in
+          Err.warning
+            ~loc
+            [ Pp.textf "Hey, you're using the cool '%s' library!" library_name
+            ; Pp.textf "How awesome is that?"
+            ]
+            ~hints:[ Pp.verbatim "Keep up the good work." ]
+        | Atom _ | List _ -> ());
+      Skip
+    | _ -> Continue);
+  ();
+  [%expect
+    {|
+    File "path/lib/dune", line 8, characters 3-17:
+    8 |    sexps_rewriter
+           ^^^^^^^^^^^^^^
+    Warning: Hey, you're using the cool 'sexps_rewriter' library!
+    How awesome is that?
+    Hint: Keep up the good work.
+    |}];
+  ()
+;;
+
+(* We test here the behavior when the input contains an invalid sexp. *)
+let syntax_error =
+  {|
+; Hey this is a comment, which we should preserve !!!
+(library
+  (name sexps_rewriter_test)
+  (flags -w +a-4-40-42-44-66 -warn-error +a)
+  (libraries
+   core
+   expect_test_helpers_core
+   sexps_rewriter
+   file_rewriter
+   parsexp)
+  ; This is another comment.
+  (inline_tests
+  (preprocess
+   (pps ppx_jane ppx_js_style -check-doc-comments)))
+|}
+  |> String.strip
+;;
+
+let%expect_test "syntax-error" =
+  let path = Fpath.v "my-dune-file" in
+  save_file ~path ~contents:syntax_error;
+  Err.For_test.protect (fun () ->
+    match Sexps_rewriter.create ~path ~original_contents:syntax_error with
+    | Ok (_ : Sexps_rewriter.t) -> assert false
+    | Error { loc; message } -> Err.raise ~loc [ Pp.textf "%s" message ]);
+  [%expect
+    {|
+    File "my-dune-file", line 14, characters 52-52:
+    14 |    (pps ppx_jane ppx_js_style -check-doc-comments)))
+
+    Error: unclosed parentheses at end of input
+    [123]
+    |}];
+  ()
+;;
