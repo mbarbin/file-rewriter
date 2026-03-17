084807d1a1b6e3ca80109d28f7d0b321e1770cdf
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-03-17 13:30:17 +0100

    Move stdlib test to the expected place

--- test/file-rewriter/test__stdlib.mli
+++
@@ -1,5 +0,0 @@
-(*_********************************************************************************)
-(*_  file-rewriter: Apply small rewrites to tweak or refactor your files          *)
-(*_  SPDX-FileCopyrightText: 2024-2026 Mathieu Barbin <mathieu.barbin@gmail.com>  *)
-(*_  SPDX-License-Identifier: Apache-2.0                                          *)
-(*_********************************************************************************)
