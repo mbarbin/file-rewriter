43d04a514f03c41a667c643d2e5a8148ef4155eb
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-03-17 13:19:29 +0100

    Use stdlib0 with wrapper

--- src/stdlib/stdlib0.ml
+++ src/stdlib/stdlib0.ml
@@ -14,4 +14,32 @@
 (*  limitations under the License.                                            *)
 (******************************************************************************)
 
-include Stdlib0
+module Code_error = Code_error0
+module Dyn = Dyn0
+module List = List0
+module String = String0
+module With_equal_and_dyn = With_equal_and_dyn0
+
+let print pp = Format.printf "%a@." Pp.to_fmt pp
+let print_dyn dyn = print (Dyn.pp dyn)
+let phys_equal a b = a == b
+let require cond = if not cond then failwith "Required condition does not hold"
+
+let require_does_raise f =
+  match f () with
+  | _ -> Code_error.raise "Did not raise." []
+  | exception e -> print_endline (Printexc.to_string e)
+;;
+
+let require_equal
+      (type a)
+      (module M : With_equal_and_dyn.S with type t = a)
+      (v1 : a)
+      (v2 : a)
+  =
+  if not (M.equal v1 v2)
+  then
+    Code_error.raise
+      "Values are not equal."
+      [ "v1", v1 |> M.to_dyn; "v2", v2 |> M.to_dyn ]
+;;
